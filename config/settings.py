d47a91d3b1d3b2aa22b7716e90ec1ac417145298
milad basery <miladbaseryy@gmail.com>
2026-08-30 12:32:45 +0330

    Fix cloudinary collectstatic compatibility with STATICFILES_STORAGE

--- config/settings.py
+++ config/settings.py
@@ -141,9 +141,9 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
