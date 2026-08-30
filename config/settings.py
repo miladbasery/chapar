f3e416c8c2193ee50118ff417b2f3e1d14630652
milad basery <miladbaseryy@gmail.com>
2026-08-30 12:29:17 +0330

    Fix cloudinary collectstatic compatibility with STATICFILES_STORAGE

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,7 @@ TIME_ZONE = 'Asia/Tehran'
 USE_I18N = True
 USE_TZ = True
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
@@ -144,10 +145,5 @@ STORAGES = {
     },
 }
 
-LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'tweet_feed'
-LOGOUT_REDIRECT_URL = 'tweet_feed'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
